3a79645f78720258af5c0aacb04b87b2553cb50b
morikentiger <kenblacksword339@gmail.com>
2024-08-18 08:12:44 +0900

    Initial Commit

---
+++ Kirby=Ore/Kirby_OreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kirby_OreApp.swift
+//  Kirby=Ore
+//
+//  Created by 森田健太 on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Kirby_OreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kirby=OreTests/Kirby_OreTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kirby_OreTests.swift
+//  Kirby=OreTests
+//
+//  Created by 森田健太 on 8/18/24.
+//
+
+import Testing
+@testable import Kirby_Ore
+
+struct Kirby_OreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
